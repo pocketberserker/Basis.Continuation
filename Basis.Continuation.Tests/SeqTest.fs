2df48c668fbb353d75bc4fc5a9ce9665b4dfd32a
pocketberserker <pocketberserker@gmail.com>
2014-02-16 14:22:40 +0900

    add Seq module

---
+++ Basis.Continuation.Tests/SeqTest.fs
@@ -0,0 +1,42 @@
+﻿namespace Basis.Continuation.Tests
+
+open NUnit.Framework
+open FsUnit
+open FsCheck
+open FsCheck.NUnit
+
+module ContSeq = Basis.Continuation.Seq
+
+[<TestFixture>]
+module SeqTest =
+
+  [<Test>]
+  let ``Continuation.Seq.fold should be equal to Seq.fold`` () =
+    check <| fun (xs: int list) ->
+      ContSeq.fold (fun acc x k -> acc + x |> k) 0 xs id = Seq.fold (+) 0 xs
+
+  [<Test>]
+  let ``Continuation.Seq.foldBack should be equal to List.foldBack`` () =
+    check <| fun (xs: int list) ->
+      ContSeq.foldBack (fun acc x k -> acc + x |> k) xs 0 id = List.foldBack (+) xs 0
+
+  [<Test>]
+  let ``Continuation.Seq.map should be equal to Seq.map`` () =
+    check <| fun (xs: int list) ->
+      let a = ContSeq.map ((*) 2) xs id |> Seq.toList
+      let b = Seq.map ((*) 2) xs |> Seq.toList
+      a = b
+
+  [<Test>]
+  let ``Continuation.Seq.concat should be equal to Seq.concat`` () =
+    check <| fun (xss: int list list) ->
+      let a = ContSeq.concat xss id |> Seq.toList
+      let b = Seq.concat xss |> Seq.toList
+      a = b
+
+  [<Test>]
+  let ``Continuation.Seq.collect should be equal to Seq.collect`` () =
+    check <| fun (xs: int list) ->
+      let a = ContSeq.collect (fun x k -> seq { yield x; yield -x } |> k) xs id |> Seq.toList
+      let b = Seq.collect (fun x -> seq { yield x; yield -x }) xs |> Seq.toList
+      a = b
---
+++ Basis.Continuation/Seq.fs
@@ -0,0 +1,40 @@
+﻿namespace Basis.Continuation
+
+module Seq =
+
+  let (|Empty|Cons|) xs =
+    if xs |> Seq.isEmpty then Empty
+    else Cons ((Seq.head xs), (Seq.skip 1 xs))
+
+  let rec fold f state xs cont =
+    match xs with
+    | Empty -> cont state
+    | Cons(x, xs) -> f state x (fun v -> fold f v xs cont)
+
+  let rec foldBack f xs state cont =
+    match xs with
+    | Empty -> cont state
+    | Cons(x, xs) -> foldBack f xs state (fun state -> f x state cont)
+
+  let cons x xs cont = seq { yield x; yield! xs } |> cont
+
+  let map f xs cont = foldBack (fun x acc k -> cons (f x) acc k) xs Seq.empty cont
+
+  let concat xss cont =
+    foldBack (fun xs acc k -> acc |> Seq.append xs |> k) xss Seq.empty cont
+
+  let collect f xs cont =
+    fold (fun acc x k -> Seq.append acc (f x k)) Seq.empty xs cont
+
+  let take count (xs: _ seq) cont =
+    let xs =
+      if count < 0 then invalidArg "count" "input should be nonnegative number."
+      elif count = 0 then Seq.empty
+      else
+        seq {
+          use e = xs.GetEnumerator()
+          for _ in 0 .. count - 1 do
+            if not (e.MoveNext()) then
+              raise <| System.InvalidOperationException "not enough elements."
+            yield e.Current }
+    cont xs
